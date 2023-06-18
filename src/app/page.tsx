17d2d79d6adef6a7b46bf0fa03eac4193216faac
Malik Teague <malikisiah214@gmail.com>
2023-06-18 16:30:54 -0700

    works with headless ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./hero";
+import MyModal from "@/components/Modal";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <MyModal />
     </div>
   );
 }
