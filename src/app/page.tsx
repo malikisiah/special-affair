fd00fb154b1eb1cef3b63221f0835b3ad92cbadf
Malik Teague <malikisiah214@gmail.com>
2023-06-18 22:35:10 -0700

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import Hero from "./hero";
-import MyModal from "@/components/Modal";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <MyModal />
     </div>
   );
 }
