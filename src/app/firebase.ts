a49436d75bfb329361701329dcc9fa8da0ecb235
Malik Teague <malikisiah214@gmail.com>
2023-06-20 13:49:04 -0700

    added env variables

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCP09nHSJIB_gEOFlVr-KpNEKUwUOHUDs",
-  authDomain: "a-special-affair.firebaseapp.com",
-  projectId: "a-special-affair",
-  storageBucket: "a-special-affair.appspot.com",
-  messagingSenderId: "788472104620",
-  appId: "1:788472104620:web:0a900406c6746985fec109",
-  measurementId: "G-RQP77YGWMK",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
