8471a4e8ced9bd9582cc06f324ce3261c6dabb9a
Malik Teague <malikisiah214@gmail.com>
2023-07-22 18:40:55 -0700

    added better stock photos

--- src/app/gallery/gallery.tsx
+++ src/app/gallery/gallery.tsx
@@ -1,54 +1,46 @@
 "use client";
 import Header from "../../components/galleryHeader";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function Gallery() {
   const team = [
     {
-      avatar:
-        "https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-      name: "Bianca Friend #1",
-      title: "Bad Bitch",
+      avatar: "/alexander-jawfox-0mqcCmuWNlc-unsplash.jpg",
     },
     {
-      avatar:
-        "https://images.unsplash.com/photo-1623605931891-d5b95ee98459?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=640&q=80",
-      name: "Bianca Friend #2",
-      title: "Spicy Latina",
+      avatar: "/ayo-ogunseinde-RrD8ypt8cjY-unsplash.jpg",
     },
     {
-      avatar:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      name: "Bianca Friend #3",
-      title: "Sexy Chocolate",
+      avatar: "/brian-lawson-PN6LflfvoVk-unsplash.jpg",
     },
     {
-      avatar:
-        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      name: "Bianca Friend #4",
-      title: "Somebody mama",
+      avatar: "/jackson-david-ikRDzgaNhIo-unsplash.jpg",
     },
     {
-      avatar:
-        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      name: "Bianca Friend #5",
-      title: "Too tired for this",
+      avatar: "/leon-liu-DhKhYwK5CFY-unsplash.jpg",
     },
     {
-      avatar:
-        "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-      name: "Bianca Friend #6",
-      title: "District Attorney",
+      avatar: "/pooja-chaudhary-bqnbKxiIGZI-unsplash.jpg",
+    },
+    {
+      avatar: "/jeffery-erhunse-0XWHaJ73wts-unsplash.jpg",
+    },
+    {
+      avatar: "/roman-laschov-gfX1D2_5j2g-unsplash.jpg",
+    },
+    {
+      avatar: "/bruce-mars-pFyKRmDiWEA-unsplash.jpg",
     },
   ];
 
   return (
     <>
-      <section className='py-14'>
-        <div className='max-w-screen-xl mx-auto px-4 md:px-8'>
+      <section className="py-14">
+        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
           <Header />
-          <div className='mt-12'>
-            <ul className='grid gap-8 sm:grid-cols-2 md:grid-cols-3'>
+          <div className="mt-12">
+            <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
               {team.map((item, idx) => (
                 <li key={idx}>
                   <motion.div
@@ -58,19 +50,16 @@ export default function Gallery() {
                       transition: { delay: idx * 0.15 },
                     }}
                   >
-                    <div className='w-full h-60 sm:h-52 md:h-56'>
-                      <img
+                    <div className="w-full h-full">
+                      <Image
+                        width={500}
+                        height={500}
+                        quality={100}
                         src={item.avatar}
-                        className='w-full h-full object-cover object-center shadow-md rounded-xl hover:btn-ghost'
-                        alt=''
+                        className="w-full h-full object-cover object-center shadow-md rounded-xl hover:btn-ghost transition hover:scale-110"
+                        alt=""
                       />
                     </div>
-                    {/* <div className='mt-4'>
-                      <h4 className='text-lg text-gray-700 font-semibold'>
-                        {item.name}
-                      </h4>
-                      <p className='text-secondary-focus'>{item.title}</p>
-                    </div> */}
                   </motion.div>
                 </li>
               ))}
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -5,11 +5,7 @@ export default function Page() {
   return (
     <>
       <Gallery />
-      <div className='flex justify-center items-center mt-12'>
-        <div className=' md:w-2/3 md:h-2/3'>
-          <Carousel />
-        </div>
-      </div>
+      <div className="flex justify-center items-center mt-12"></div>
     </>
   );
 }
